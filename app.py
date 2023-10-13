9468b8044e6e19b7fe5d2bd35fb8e542cfbfe8f2
Batuhan Topçu <topcubatuhan00@hotmail.com>
2023-10-13 17:34:05 +0300

    random forest algorith ex added

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+
 malbourne_file_path = './melb_data.csv'
 
 # read csv
@@ -44,4 +52,15 @@ melbourne_model = DecisionTreeRegressor()
 melbourne_model.fit(train_X, train_y)
 
 val_predictions = melbourne_model.predict(val_X)
-print(mean_absolute_error(val_y, val_predictions))
+# print(mean_absolute_error(val_y, val_predictions))
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    may_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    # print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, may_mae))
+
+from sklearn.ensemble import RandomForestRegressor
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
